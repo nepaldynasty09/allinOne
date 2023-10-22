84fd6e6394dadaaccb8ec3ee7267c4b485575a3c
Bivek Nepal <115235822+nepaldynasty09@users.noreply.github.com>
2023-10-22 21:06:55 +0545

    added all the subgames file/folder

---
+++ number guesser/script.js
@@ -0,0 +1,111 @@
+let usedTries=0;
+let compCHoice;
+let initialTries=10;
+let butt=document.getElementById("btn");
+let score=document.getElementById("tries");
+let resetBtn=document.getElementById("reset-btn");
+let hints=document.getElementById("aboutgame");
+let hintbttn=document.getElementById("hint");
+let hidebttn=document.getElementById("hide");
+setBoard();
+function setBoard(){
+
+    score.innerHTML=`Tries Remaining: ${initialTries}`
+    compCHoice=Math.floor(Math.random()*100);
+    message.innerHTML=`Please guess a number`
+}
+let levelSelectButtons = document.querySelectorAll("#level");
+let gameContainer = document.getElementById("game-container");
+
+
+levelSelectButtons.forEach(function(button) {
+  button.addEventListener("click", function() {
+    
+    levelSelectButtons.forEach(function(button) {
+      button.style.display = "none";
+    });
+    showGame();
+  });
+});
+
+function showGame() {
+  gameContainer.style.display = "block";
+}
+function setLevel(level){
+    let score=document.getElementById("tries")
+    initialTries = (level == 1) ? 10 : (level == 2) ? 5 : (level == 3) ? 3 : 0;
+    butt.style.display="inline";
+    score.innerHTML=`Tries Remaining: ${initialTries}`
+    
+}
+
+function handleEnterKey(event) {
+    if (event.keyCode === 13) {
+      checkNumber();
+    }
+  }
+  document.getElementById("input").addEventListener("keydown", handleEnterKey);
+
+function checkNumber(){
+    
+    
+    let userChoice=parseInt(document.getElementById("input").value);
+    let message=document.getElementById("message")
+    usedTries++;
+    if(!userChoice)
+      message.innerHTML=`Please input a number from 1-100`
+    else if(compCHoice!==userChoice){
+        initialTries--;
+        if(compCHoice>userChoice){
+            message.innerHTML=`Higher than ${userChoice}`
+            
+        }
+        else if((compCHoice<userChoice)){
+            message.innerHTML=`Lower than ${userChoice} `
+        }
+        score.innerHTML=`Tries Remaining: ${initialTries}`;
+        document.getElementById("input").value = "";
+        checkForLoss();
+    }
+    else if(compCHoice===userChoice){
+        
+        score.innerHTML=`You won`;
+         message.innerHTML=`Congrats, you have guessed the number in ${usedTries} tries`
+         butt.style.display="none"; 
+         resetBtn.style.display="block";
+         document.getElementById("input").value = "";
+    }
+}
+
+function resetGame(){
+    levelSelectButtons.forEach(function(button) {
+        button.style.display ="inline";
+      });
+      gameContainer.style.display = "none";
+      document.getElementById("input").disabled = false;
+      setBoard();
+      document.getElementById("input").value = "";
+      usedTries=0;
+}
+function checkForLoss(){
+    if(initialTries==0)
+    {
+        message.innerHTML=`Tries Over You lost, the number was ${compCHoice}`;
+        score.innerHTML=`Lost`;
+        document.getElementById("input").disabled = true;
+        resetBtn.style.display="block";
+        butt.style.display="none"; 
+    }
+}
+
+function showHints(){
+  hints.style.display="inline";
+  hintbttn.style.display="none";
+  hidebttn.style.display="inline"
+};
+
+function hideHints(){
+  hints.style.display="none";
+  hidebttn.style.display="none"
+  hintbttn.style.display="inline";
+}
---
+++ rock paper scissor v2/script.js
@@ -0,0 +1,102 @@
+// script.js
+let userWins = 0;
+let computerWins = 0;
+let draws = 0;
+
+// function playSound(choice) {
+//     const audio = new Audio(`${choice}.mp3`);
+//     audio.play();
+// }
+
+function displayChoice(elementId, choice) {
+    const choiceElement = document.getElementById(elementId);
+
+    while (choiceElement.firstChild) {
+        choiceElement.firstChild.remove();
+    }
+ 
+}
+
+function updateScoreboard() {
+    const scoreboardElement = document.getElementById("scoreboard");
+    scoreboardElement.innerHTML = `
+        <span style="background-color: blue;">User: ${userWins}</span> |
+        <span style="background-color: red;">Computer: ${computerWins}</span> |
+        <span style="background-color: yellow; color: black;">Draws: ${draws}</span>`;
+}
+
+function userChoice(ch) {
+    choice = ch;
+
+    const user = choice === 1 ? "rock" : choice === 2 ? "paper" : choice === 3 ? "scissor" : null;
+
+    if (user === null) {
+    alert("Please enter a valid input (1-3)");
+    return;
+}
+ 
+
+
+    const compchoice = Math.floor(Math.random() * 3);
+    const ai = compchoice === 0 ? "rock" : compchoice === 1 ? "paper" : "scissor";
+
+
+    displayResult(user, ai);
+}
+
+function displayResult(user, ai) {
+    let resultMessage = "";
+    if (ai == user) {
+        resultMessage = `It's a draw! You both chose ${user}. Wanna try again? 😊`;
+        draws++;
+    } else if (
+        (ai == "rock" && user == "scissor") ||
+        (ai == "scissor" && user == "paper") ||
+        (ai == "paper" && user == "rock")
+    ) {
+        resultMessage = `Your choice: ${user}, Computer's choice: ${ai}. You lost. Better luck next time! 🙁`;
+        computerWins++;
+    } else {
+        resultMessage = `Your choice: ${user}, Computer's choice: ${ai}. Congratulations, you won! 🎉`;
+        userWins++;
+    }
+
+    updateScoreboard();
+
+
+    const resultElement = document.createElement("div");
+    resultElement.classList.add("result-message");
+    resultElement.textContent = resultMessage;
+    document.body.appendChild(resultElement);
+
+    setTimeout(() => {
+        resultElement.remove();
+    }, 3000);
+}
+
+// Function to show the game board and hide the "Play Game" button
+function showGame() {
+    const playButton = document.getElementById("play-button");
+    const gameContainer = document.getElementById("game-container");
+
+    playButton.style.display = "none";
+    gameContainer.style.display = "block";
+}
+
+// Add an event listener to the "Play Game" button
+const playButton = document.getElementById("play-button");
+playButton.addEventListener("click", showGame);
+
+const resetButton = document.getElementById("reset-button");
+resetButton.addEventListener("click", resetGame);
+
+function resetGame() {
+    userWins = 0;
+    computerWins = 0;
+    draws = 0;
+    updateScoreboard();
+    const choiceImages = document.querySelectorAll(".choice-image.show");
+    choiceImages.forEach((image) => {
+        image.classList.remove("show");
+    });
+}
